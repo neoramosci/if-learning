96a1e01e5434d44e7d268fb1d19585a6bcc3e958
Irineo Ramos <irineo.ramos@codeimmersives.com>
2021-02-22 11:00:20 -0600

    all ifs pass.

--- main.js
+++ main.js
@@ -2,10 +2,49 @@
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
 
+ //question 1
+ function isItLong(str){
+  if(str.length(20)){
+    return ("That's a long string!");
+  }
+ }
 
+// question 2 
+function isItMedium(str){
+if(str.length >= 10 && str.length <=20){
+return "That's a regular sized string!";
+}
+}
 
+//question 3 
+function isItShort(stf){
+  if(str.length < 10){
+    return "That's a small string!";
+  } else {
+    return "Thats not a small string!";
+  }
+}
 
+// question 4 
+function howLongIsMyString(str){
+if(str.length > 20){
+  return "That's a long string!";
+} else if (str.length >= 10 && str.length <=20){
+  return "That's a regular sized string!";
+} else if (str.length < 10){ 
+  return "That's a small string!";
+}
+}
 
+// question 5 
+function instructorHeight(str){
+  if(str ==="neo"){
+    return 62;
+  } else if (str === "james"){
+  } else {
+    return "I don't now that instructor!";
+  }
+}
 
 /**********************************
  *      OUR CODE GOES BELOW.      *
